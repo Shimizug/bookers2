67c27f9a919421e8d8132c944faad99bc1d709e1
EC2 Default User <ec2-user@ip-172-31-35-94.ap-northeast-1.compute.internal>
2022-06-15 09:45:01 +0000

    [Add]devise機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
- 
+# devise を使用する際に URL として users を含むことを示す
+  devise_for :users
 # homesコントローラのルーティング
   get 'homes/top'
-  get 'homes/about'
+  get 'homes/about', as: 'about'
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
